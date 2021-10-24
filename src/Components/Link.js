45413fd9f0a70fd905b3554a291c90aab8fc455c
Diego Reinoso <diego.reinoso@sansano.usm.cl>
2021-10-24 19:43:32 -0300

    ✨  Chatbot Styling

---
+++ src/Components/Link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Link = () => {
+
+    return (  
+        <div>
+            <a href="http://localhost:8360">Apreta aquí para iniciar la simulación</a> 
+        </div>
+    );
+}
+ 
+export default Link;
