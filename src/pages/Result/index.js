e77b88ff9dd43db9df5a747a0a995b76956919ef
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-10-25 23:58:20 -0300

    💄 Acordeon y progress bars!

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -2,12 +2,16 @@ import Biases from "./biases.js";
 import FlipCard from "../../Components/FlipCard/FlipCard.js";
 import ButtonAppBar from "../../Components/NavBar/AppBar.js";
 
-
 import * as React from 'react'
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles'
-import { Typography } from "@mui/material";
+import {Typography, LinearProgress} from "@mui/material";
+import {makeStyles} from "@mui/styles"
+import MuiAccordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -25,8 +29,32 @@ const ResultGrid = styled(Grid)`
   align-items: center;
 `;
 
+const Accordion = styled((props) => (
+  <MuiAccordion elevation={2} {...props}/>
+))(({ theme }) => ({
+  borderRadius:5,
+}));
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    marginBottom: 15,
+  },
+  column: {
+    flexBasis: '10%',
+  },
+  column2: {
+    flexBasis: '80%',
+    justifyContent:"",
+    alignContent: "right",
+  },
+}))
+
+
 
 export default function Result(bias, result1, result2, result3) {
+
+  const classes = useStyles();
 
   bias = "maternal";
 
@@ -37,7 +65,6 @@ export default function Result(bias, result1, result2, result3) {
 
   return (
     <>
-    
     <ResultGrid container columnSpacing={3}>
     <ButtonAppBar/>
       <Grid item xs={4}>
@@ -46,9 +73,74 @@ export default function Result(bias, result1, result2, result3) {
         </Item>
       </Grid>
       <Grid item xs={8}>
-        <Item sx={{height:"70vh", p:"30px", borderRadius: 5}}>
-          <Typography sx={{ textAlign: 'left', fontSize: 32}}>Resultados</Typography>
-
+        <Item sx={{height:"70vh", p:"30px", borderRadius: 5, backgroundColor:"#fff"}}>
+          <Typography sx={{ textAlign: 'left', fontSize: 32, mb:5}}>Resultados</Typography>
+          <div className={classes.root}>
+          <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+              <div className={classes.column}>
+                <Typography>Simulacion 1</Typography>
+              </div>
+              <div className={classes.column2} style={{margin:"10px"}}>
+              <LinearProgress variant="determinate" value={result1}/>
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          </div>
+          <div className={classes.root}>
+          <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+              <div className={classes.column}>
+                <Typography>Simulacion 2</Typography>
+              </div>
+              <div className={classes.column2} style={{margin:"10px"}}>
+              <LinearProgress variant="determinate" value={result2}/>
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          </div>
+          <div className={classes.root}>
+          <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+              <div className={classes.column}>
+                <Typography>Simulacion 3</Typography>
+              </div>
+              <div className={classes.column2} style={{margin:"10px"}}>
+              <LinearProgress variant="determinate" value={result3}/>
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          </div>
         </Item>
       </Grid>
     </ResultGrid>
