d97f22175d2f1920bc70e672fec5c52f3a08d898
Nicolás Maldonado <nicolas.maldonado.13@sansano.usm.cl>
2021-10-27 19:07:07 -0300

    Use Nobias icon in navbar and progressbar working

--- src/Components/NavBar/AppBar.js
+++ src/Components/NavBar/AppBar.js
@@ -1,29 +1,31 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "../../logoNB.png";
+import "./style.scss";
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <AppBar 
-        position="fixed"
-        sx={{background:'#7879F1', width:"100vw"}}
-      >
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ background: "#7879F1", width: "100vw" }}>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2}}
+            sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <img src={MenuIcon} alt="MenuIcon" className="logo" />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign:"left"}}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "left" }}
+          >
             No Bias
           </Typography>
         </Toolbar>
--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,151 +1,157 @@
 import Biases from "./biases.js";
 import FlipCard from "../../Components/FlipCard/FlipCard.js";
 import ButtonAppBar from "../../Components/NavBar/AppBar.js";
+import ProgressBar from "../../Components/Progress/progress.js";
 
-import * as React from 'react'
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles'
-import {Typography, LinearProgress} from "@mui/material";
-import {makeStyles} from "@mui/styles"
-import MuiAccordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import MuiAccordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.primary,
   margin: 30,
-  boxShadow:3,
+  boxShadow: 3,
 }));
 
 const ResultGrid = styled(Grid)`
   width: 100%;
-  justify-content:space-around;
+  justify-content: space-around;
   align-items: center;
 `;
 
-const Accordion = styled((props) => (
-  <MuiAccordion elevation={2} {...props}/>
-))(({ theme }) => ({
-  borderRadius:5,
-}));
+const Accordion = styled((props) => <MuiAccordion elevation={2} {...props} />)(
+  ({ theme }) => ({
+    borderRadius: 5,
+  })
+);
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginBottom: 15,
   },
   column: {
-    flexBasis: '10%',
+    flexBasis: "10%",
   },
   column2: {
-    flexBasis: '80%',
-    justifyContent:"",
+    flexBasis: "80%",
+    justifyContent: "",
     alignContent: "right",
   },
-}))
-
-
+}));
 
 export default function Result(bias, result1, result2, result3) {
-
   const classes = useStyles();
 
   bias = "maternal";
 
-  result1 =  10
-  result2 =  50
-  result3 =  80
-
+  result1 = 10;
+  result2 = 50;
+  result3 = 80;
 
   return (
     <>
-    <ResultGrid container columnSpacing={3}>
-    <ButtonAppBar/>
-      <Grid item xs={4}>
-        <Item style={{background: 'transparent', boxShadow:'none'}}>
-        <FlipCard card={Biases[bias]}></FlipCard>
-        </Item>
-      </Grid>
-      <Grid item xs={8}>
-        <Item sx={{height:"70vh", p:"30px", borderRadius: 5, backgroundColor:"#fff"}}>
-          <Typography sx={{ textAlign: 'left', fontSize: 32, mb:5}}>Resultados</Typography>
-          <div className={classes.root}>
-          <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-              <div className={classes.column}>
-                <Typography>Simulacion 1</Typography>
-              </div>
-              <div className={classes.column2} style={{margin:"10px"}}>
-              <LinearProgress variant="determinate" value={result1}/>
-              </div>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          </div>
-          <div className={classes.root}>
-          <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-              <div className={classes.column}>
-                <Typography>Simulacion 2</Typography>
-              </div>
-              <div className={classes.column2} style={{margin:"10px"}}>
-              <LinearProgress variant="determinate" value={result2}/>
-              </div>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          </div>
-          <div className={classes.root}>
-          <Accordion>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            >
-              <div className={classes.column}>
-                <Typography>Simulacion 3</Typography>
-              </div>
-              <div className={classes.column2} style={{margin:"10px"}}>
-              <LinearProgress variant="determinate" value={result3}/>
-              </div>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          </div>
-        </Item>
-      </Grid>
-    </ResultGrid>
+      <ResultGrid container columnSpacing={3}>
+        <ButtonAppBar />
+        <Grid item xs={4}>
+          <Item style={{ background: "transparent", boxShadow: "none" }}>
+            <FlipCard card={Biases[bias]}></FlipCard>
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item
+            sx={{
+              height: "70vh",
+              p: "30px",
+              borderRadius: 5,
+              backgroundColor: "#fff",
+            }}
+          >
+            <Typography sx={{ textAlign: "left", fontSize: 32, mb: 5 }}>
+              Resultados
+            </Typography>
+            <div className={classes.root}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <div className={classes.column}>
+                    <Typography>Simulacion 1</Typography>
+                  </div>
+                  <div className={classes.column2} style={{ margin: "10px" }}>
+                    <ProgressBar result={result1} />
+                  </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div className={classes.root}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <div className={classes.column}>
+                    <Typography>Simulacion 2</Typography>
+                  </div>
+                  <div className={classes.column2} style={{ margin: "10px" }}>
+                    <ProgressBar result={result2} />
+                  </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div className={classes.root}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <div className={classes.column}>
+                    <Typography>Simulacion 3</Typography>
+                  </div>
+                  <div className={classes.column2} style={{ margin: "10px" }}>
+                    <ProgressBar result={result3} />
+                  </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          </Item>
+        </Grid>
+      </ResultGrid>
     </>
   );
 }
-
-
