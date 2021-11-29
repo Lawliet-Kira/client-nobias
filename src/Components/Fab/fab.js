ec1bc31cf718c9032c693edaf38127f23dfa14fd
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-11-29 20:34:06 -0300

    🚧 Tooltip funcionando

--- src/Components/Fab/fab.js
+++ src/Components/Fab/fab.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
-import { Typography } from '@mui/material';
+import { Tooltip, Typography, tooltipClasses } from '@mui/material';
+import { styled } from '@mui/system';
 
 const fabStyle ={
     position: 'absolute',
@@ -14,6 +15,19 @@ const fabStyle ={
         bgcolor: "#6B6BEF"
     }
 };
+
+const HtmlTooltip = styled(({className, ...props}) => (
+    <Tooltip {...props} classes={{popper: className}} />
+))(({theme}) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: "fff",
+        color: "0000",
+        maxWidth: 220,
+        fontSize: 11,
+        border: '1px solid #dadde9',
+    },
+}));
+
 
 
 export default function FAB() {
@@ -27,10 +41,21 @@ export default function FAB() {
 
 
   return (
-    <Box sx={{ position: 'absolute', bottom:20, right: 20 }}>
-      <Fab sx = {fabs.sx} aria-label={fabs.label} color={fabs.color} >
-        <Typography fontSize={24} >?</Typography>
-      </Fab>
-    </Box>
+    <div>
+        <Box sx={{ position: 'absolute', bottom:20, right: 20 }}>
+            <HtmlTooltip
+                title={
+                    <React.Fragment>
+                        <Typography color='inherit'> Ayuda </Typography>
+                        <b>{"Revisaste tus resultados?"}</b><br/> {"Prueba con hacer clic en el logo de No Bias para navegar al inicio de la aplicacion."}
+                    </React.Fragment>
+                }
+            >
+            <Fab sx = {fabs.sx} aria-label={fabs.label} color={fabs.color} >
+                <Typography fontSize={24} >?</Typography>
+            </Fab>
+            </HtmlTooltip>
+        </Box>
+    </div>
   );
 }
