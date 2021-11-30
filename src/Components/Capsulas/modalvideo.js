307d14a995ca5e85e82ff6c39fa169bc09681edd
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-11-30 02:26:11 -0300

    🚧 ModalVideo Component

--- src/Components/Capsulas/modalvideo.js
+++ src/Components/Capsulas/modalvideo.js
@@ -1,22 +1,63 @@
-import React,{useState} from 'react'
-import ReactDOM from 'react-dom'
-import ModalVideo from 'react-modal-video'
-import '../../../node_modules/react-modal-video/scss/modal-video.scss';
+import * as React from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
-const ModalVid = () => {
+import YouTube from 'react-youtube';
 
-	const [isOpen, setOpen] = useState(false)
+const style = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: "640",
+	height: "60vh",
+	bgcolor: 'background.paper',
+	borderRadius: "30",
+	border: '2px solid background.paper',
+	boxShadow: 20,
+	p: 4,
+};
+
+
+
+const VideoModal = ({bias, foto, explicacion, video}) => {
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	const opts = {
+		height: '390',
+		width: '640',
+		playerVars: {
+		  // https://developers.google.com/youtube/player_parameters
+		  autoplay: 1,
+		},
+	  };
+
 
 	return (
-		<React.Fragment>
-			<ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="L61p2uyiMSo" onClose={() => setOpen(false)} />
-
-			<button className="btn-primary" onClick={()=> setOpen(true)}>VIEW DEMO</button>
-		</React.Fragment>
-	)
-}
-
-ReactDOM.render(
-  <ModalVid />,
-    document.getElementById('root')
-)
+		<div>
+			<img alt="nombre" src={foto} width={400} maxHeight={300} onClick={()=> setOpen(true)} />
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>
+					<YouTube videoId={video} opts={opts} />
+					<br/>
+					<Typography variant="h4" component="h2" sx={{textAlign:"left"}}>
+						{bias} Bias
+					</Typography>
+					<Typography fontSize={16} sx={{ mt: 2 }}>
+						{explicacion}
+					</Typography>
+				</Box>
+			</Modal>
+		</div>
+	);
+	}
+export default VideoModal;
