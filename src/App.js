6c9dea8c1f728d5070ccd88c6092eafe2bf8079d
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-11-30 02:27:44 -0300

    🎨 Mejoras en Capsulas

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           No Bias!
         </a> */}
         {/* <SimpleForm /> */}
-        <Result />
+        <Capsulas />
       </header>
     </div>
   );
--- src/pages/Capsulas/Capsulas.js
+++ src/pages/Capsulas/Capsulas.js
@@ -3,15 +3,19 @@ import * as React from "react";
 // MUI Components
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
-import { Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
 
 // Custom components
 import ButtonAppBar from "../../Components/NavBar/AppBar.js";
-//import ModalVid from '../../Components/Capsulas/modalvideo.js'
-import { CustomPlaceholder } from 'react-placeholder-image';
+import FAB from "../../Components/Fab/fab.js"
 
-// Images y Videos
+import MiniAttribution from '../../assets/Capsulas/Img/MiniaturaAttribution.jpg';
+import MiniMaternal from "../../assets/Capsulas/Img/MiniaturaMaternal.jpg";
+import MiniPerformance from "../../assets/Capsulas/Img/MiniaturaPerformance.jpg";
+import MiniUnconscious from "../../assets/Capsulas/Img/MiniaturaUnconscious.jpg";
 
+
+import VideoModal from "../../Components/Capsulas/modalvideo";
 
 
 const ResultGrid = styled(Grid)`
@@ -24,7 +28,7 @@ export default function Capsulas() {
 
 
     return (
-        <ResultGrid container columnSpacing = {3} rowSpacing={4}>
+        <ResultGrid container columnSpacing = {4} rowSpacing={5}>
             <ButtonAppBar />
             {/* Primera Fila */}
             <Grid item xs = {12}> 
@@ -33,78 +37,43 @@ export default function Capsulas() {
                 </Typography>
             </Grid>
             <Grid item xs={4}>
-                <CustomPlaceholder
-                width={400}
-                height={300}
-                backgroundColor="#7879F1"
-                textColor="#ffffff"
-                text="Affinity Bias"
-                />
-                <Typography>
+                <VideoModal bias="Affinity" foto={MiniPerformance} explicacion="asdfhjkasjkhdf" video="2g811Eo7K8U"/>
+                <Typography fontSize={24} fontWeight={700}>
                     Affinity Bias
                 </Typography>
             </Grid>
             <Grid item xs={4}>
-                <CustomPlaceholder
-                width={400}
-                height={300}
-                backgroundColor="#7879F1"
-                textColor="#ffffff"
-                text="Likeability Bias"
-                />
-                <Typography>
+                <VideoModal bias="Likeability" foto={MiniPerformance} explicacion="asdfhjkasjkhdf" video="2g811Eo7K8U"/>
+                <Typography fontSize={24} fontWeight={700}>
                     Likeability Bias
                 </Typography>
             </Grid>
             <Grid item xs={4}>
-                <CustomPlaceholder
-                width={400}
-                height={300}
-                backgroundColor="#7879F1"
-                textColor="#ffffff"
-                text="Performance Bias"
-                />
-                <Typography>
+                <VideoModal bias="Performance" foto={MiniPerformance} explicacion="asdfhjkasjkhdf" video="2g811Eo7K8U"/>
+                <Typography fontSize={24} fontWeight={700}>
                     Performance Bias
                 </Typography>
             </Grid>
             {/* Segunda Fila */}
             <Grid item xs={4}>
-                <CustomPlaceholder
-                width={400}
-                height={300}
-                backgroundColor="#7879F1"
-                textColor="#ffffff"
-                text="Unconscious Bias"
-                />
-                <Typography>
+                <VideoModal bias="Unconscious" foto={MiniUnconscious} explicacion="asdfhjkasjkhdf" video="2g811Eo7K8U"/>
+                <Typography fontSize={24} fontWeight={700}>
                     Unconscious Bias
                 </Typography>
             </Grid>
             <Grid item xs={4}>
-                <CustomPlaceholder
-                width={400}
-                height={300}
-                backgroundColor="#7879F1"
-                textColor="#ffffff"
-                text="Maternal Bias"
-                />
-                <Typography>
+                <VideoModal bias="Maternal" foto={MiniMaternal} explicacion="asdfhjkasjkhdf" video="2g811Eo7K8U"/>
+                <Typography fontSize={24} fontWeight={700}>
                     Maternal Bias
                 </Typography>
             </Grid>
             <Grid item xs={4}>
-                <CustomPlaceholder
-                width={400}
-                height={300}
-                backgroundColor="#7879F1"
-                textColor="#ffffff"
-                text="Attribution Bias"
-                />
-                <Typography>
+                <VideoModal bias="Attribution" foto={MiniAttribution} explicacion="asdfhjkasjkhdf" video="2g811Eo7K8U"/>
+                <Typography fontSize={24} fontWeight={700}>
                     Attribution Bias
                 </Typography>
             </Grid>
+            <FAB text_bold={"Ya viste todas las capsulas?"} text_normal={"Prueba con hacer clic en el logo de No Bias para navegar al inicio de la aplicacion."}/>
         </ResultGrid>
     );
 }
