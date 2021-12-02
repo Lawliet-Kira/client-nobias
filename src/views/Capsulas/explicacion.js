328bb384fac075c722dbbea23a56f3a97e64ceef
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-12-01 23:50:27 -0300

    ✨ Explicaciones para capsulas

---
+++ src/views/Capsulas/explicacion.js
@@ -0,0 +1,22 @@
+const Explicaciones = {
+    performance: {
+        descripcion:'Las personas tienden a pensar en ocasiones que las mujeres son menos competentes que los hombres lo que los conduce a tomarlas con menor seriedad y asumir que tienen un menor poder y estatus. Al pedir un café el sujeto se encuentra realizando esto mismo, rebajando el estatus y poder de la persona por el hecho de ser mujer y asumiendo que no tiene la capacidad de liderar la empresa. '
+    },
+    affinity: {
+        descripcion: 'Cuando alguien descarte a un candidato debido a como luce, pídele que sea más específico. Si su pensamiento se reduce a que "ella es diferente", señala que "diferente" puede ser algo bueno. Propón que busque a alguien que contribuya a la dinámica del equipo, un "añadido cultural", en lugar de a alguien que simplemente encaje en él. Como solución a más largo plazo, solicita que se utilice un conjunto de criterios estandarizados para todas las contrataciones. Esto reduce el sesgo al minimizar las evaluaciones subjetivas.'
+    },
+    attribution: {
+        descripcion:'Las mujeres ganan en promedio un 27% menos que un hombre por hacer el mismo trabajo. Al decidir hablar con el jefe, el joven está ayudando a combatir esta cifra para lograr una mayor igualdad. Pero lamentablemente, al no estar de acuerdo con entregar parte de su bono, si bien se preocupa porque ambos ganen lo mismo, al momento de actuar no ayuda en lo absoluto.'
+    },
+    likeability:{
+        descripcion: "Un estudio demostró que las mujeres frecuentemente reciben comentarios negativos por su forma de hablar mientras que en el caso de los hombres esto no ocurre tan a menudo. Se tiende a generar el estereotipo de que las mujeres deben hablar de manera tierna y educada. Cuando notes que alguien critica a una mujer según su forma de hablar sugiere que se detenga a pensar si realmente se encuentra criticando la manera de hablar o se encuentra criticando a la mujer que habla de esa manera."
+    },
+    unconscious:{
+        descripcion:"Es común en nuestro día a día asociar inconscientemente conceptos al género de las personas. Comentarios como 'ese trago es de mina' o 'escoge algo más de hombre' fortalecen esta idea y lamentablemente, generan esquemas de cómo una mujer o un hombre debe comportarse cuando son ellos mismos quienes deben decidir cómo hacerlo. Al no hacer nada, se aporta indirectamente a que esto ocurra."
+    },
+    maternal:{
+        descripcion:'Tratar de poco profesional a la mujer puede herir su reputación y este tipo de interrupciones no tienen relación alguna con su desempeño, esta es una situación que le puede ocurrir a cualquiera y se encuentra fuera del control de uno mismo. Al estar de acuerdo, se está apoyando este comportamiento.'
+    }
+}
+
+export default Explicaciones;
