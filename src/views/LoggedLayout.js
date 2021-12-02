02797abfb870a1cbbf6b635cd647648dfdafdd30
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-12-02 01:49:31 -0300

    🚑️ Router

--- src/views/LoggedLayout.js
+++ src/views/LoggedLayout.js
@@ -5,9 +5,7 @@ import Dashboard from '../Components/Dashboard';
 const LoggedLayout = (props) => {
 
     return (  
-       <Router>
         <Dashboard {...props} />
-       </Router>
     );
 }
  
